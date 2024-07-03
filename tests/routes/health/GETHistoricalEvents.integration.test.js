b37d4dc7f48869b55677bc040e2bbcff4539c537
wykr0 <manuel.cruces@sansano.usm.cl>
2024-07-03 19:24:03 -0400

    Segundo Concepto

---
+++ tests/routes/health/GETHistoricalEvents.integration.test.js
@@ -0,0 +1,48 @@
+import request from 'supertest'
+import { server, app } from '../../../src/index'
+
+
+describe('GET /api/history/:ocurrence', () => {
+    afterAll(() => {
+        server.close()
+    })
+    /**
+     * Primer Test
+     */
+
+    test('Solicitud de Ocurrence ac', async () => {
+        const response = await request(app.callback()).get('/api/history/ac')
+        expect(response.status).toBe(200)
+        expect(response.body.date).toBeGreaterThan(0)
+    })
+
+    /**
+     * Segundo Test
+     */
+
+    test('Solicitud de Ocurrence dc', async () => {
+        const response = await request(app.callback()).get('/api/history/dc')
+        expect(response.status).toBe(200)
+        //expect(response.date).toBeLessThan(0)
+    })
+
+    /**
+     * Tercer Test
+     */
+    test('Solicitud con :ocurrence es un string de largo = 2, con caracteres alfanuméricos o solo númericos', async () => {
+        const response = await request(app.callback()).get('/api/history/22')
+        expect(response.status).toBe(400)
+        expect(response.body).toEqual({ message: 'Solo se aceptan caracteres no numéricos' })
+    })
+
+    /**
+     * Cuarto Test
+     */
+    test('Solicitud con :ocurrence es un string de largo != 2', async () => {
+        const response = await request(app.callback()).get('/api/history/acdc')
+        expect(response.status).toBe(400)
+        expect(response.body).toEqual({ message: 'El input debe ser ac o dc' })
+    })
+})
+
+
